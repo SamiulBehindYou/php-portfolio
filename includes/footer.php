6a72d61974ee7e976c6885a6946d1fc70569673d
SAMIUL ISLAM <124153734+SamiulBehindYou@users.noreply.github.com>
2024-11-06 11:23:35 +0600

    update links

--- includes/footer.php
+++ includes/footer.php
@@ -30,21 +30,21 @@
         Scripts
     ***********************************-->
     <!-- Required vendors -->
-    <script src="../../php-portfolio/assets/vendor/global/global.min.js"></script>
-	<script src="../../php-portfolio/assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-	<script src="../../php-portfolio/assets/vendor/chart.js/Chart.bundle.min.js"></script>
-    <script src="../../php-portfolio/assets/js/custom.min.js"></script>
-	<script src="../../php-portfolio/assets/js/deznav-init.js"></script>
-	<script src="../../php-portfolio/assets/vendor/owl-carousel/owl.carousel.js"></script>
+    <script src="../assets/vendor/global/global.min.js"></script>
+	<script src="../assets/vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+	<script src="../assets/vendor/chart.js/Chart.bundle.min.js"></script>
+    <script src="../assets/js/custom.min.js"></script>
+	<script src="../assets/js/deznav-init.js"></script>
+	<script src="../assets/vendor/owl-carousel/owl.carousel.js"></script>
 	
 	<!-- Chart piety plugin files -->
-    <script src="../../php-portfolio/assets/vendor/peity/jquery.peity.min.js"></script>
+    <script src="../assets/vendor/peity/jquery.peity.min.js"></script>
 	
 	<!-- Apex Chart -->
-	<script src="../../php-portfolio/assets/vendor/apexchart/apexchart.js"></script>
+	<script src="../assets/vendor/apexchart/apexchart.js"></script>
 	
 	<!-- Dashboard 1 -->
-	<script src="../../php-portfolio/assets/js/dashboard/dashboard-1.js"></script>
+	<script src="../assets/js/dashboard/dashboard-1.js"></script>
 	<script>
 		function carouselReview(){
 			/*  testimonial one function by = owl.carousel.js */
